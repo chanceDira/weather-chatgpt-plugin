c0a937545a2a8a086da77ee6e25a89993486b393
chanceDira <chancedesire60@gmail.com>
2023-05-19 07:21:46 +0200

    feat: added live link in manifest file

--- src/routers/get-weather.js
+++ src/routers/get-weather.js
@@ -7,7 +7,7 @@ router.get("/", async (req, res) => {
   const { search } = req.query;
   try {
     const api = {
-      key: "d91e5af2517572deed4392e96dd7c09a",
+      key: process.env.API_KEY,
       base: "https://api.openweathermap.org/data/2.5/"
     }
     const response = await axios.get(`${api.base}weather?q=${search}&units=metric&APPID=${api.key}`)
